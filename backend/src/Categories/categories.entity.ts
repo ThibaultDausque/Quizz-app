0af1a263997868db5fcbe281e34e9905dce2e51e
Hyeminah <144677544+Hyeminah@users.noreply.github.com>
2024-01-29 14:58:15 +0100

    Back/modules (#41)

---
+++ backend/src/Categories/categories.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Question } from 'src/questions/questions.entity';
+import { UUID } from 'crypto';
+
+@Entity()
+export class Categories {
+	@PrimaryGeneratedColumn()
+	id: UUID;
+
+	@Column({ length: 500 })
+	name: string;
+
+	@Column('text')
+	description: string;
+
+	@OneToMany(() => Question, (question) => question.categories)
+	questions: Question[]
+
+}
