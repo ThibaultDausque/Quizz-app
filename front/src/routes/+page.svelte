13027c90af37d185ee890f3ff914a87e5bf7017d
Steph4477 <110528810+Steph4477@users.noreply.github.com>
2024-01-30 13:39:52 +0100

    Animpts (#51)

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,7 +1,25 @@
 <script lang="ts">
-    import LogicQuiz from "../components/LogicQuiz.svelte";
+    import Card from "../components/Card.svelte";
 </script>
 
-   <LogicQuiz />
-
-
+<div class="card">
+<a href="/capitals">capital quiz
+</a>
+</div>
+<style>
+    .card {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 40px;
+        width: 300px;
+        height: 300px;
+        background-color: #f5f5f5;
+        box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.75);
+        margin-top: 15vh;
+    }
+    a {
+        text-decoration: none;
+        color: black;
+    }
+</style>
