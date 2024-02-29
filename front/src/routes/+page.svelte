3129ae957884745e4cc20b3cbf52e857ff0e82f5
Steph4477 <110528810+Steph4477@users.noreply.github.com>
2024-02-29 16:01:41 +0100

    insert card for category in home page (#107)
    Co-authored-by: Steph4477 <morelstephan4026@neuf.fr>

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,35 +1,80 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { load } from "$lib/utils";
-
+  import CardHome from "$lib/components/CardHome.svelte";
+  import Button from "$lib/components/Button.svelte";
+  let category: any = {};
   let categories: string | any[] = [];
 
   onMount(async () => {
     categories = await load();
-
   });
 </script>
 
-{#if categories.length === 0}
-  <p>pas de quizz disponible</p>
-  <a href="./form">
-    <p>proposer un quiz ?</p>
-  </a>
-{:else}
-  <h1>Categories</h1>
-  <ul>
-    {#each categories as category (category.id)}
-      <li>
-        <a href={`./categories/name/${category.name}`}>
-          <p>Nom: {category.name}</p>
-        </a>
-        <p>Description: {category.description}</p>
-      </li>
-    {/each}
-    <button>
-      <a href="./form">
-        <p>proposer un quiz ?</p>
-      </a>
-    </button>
-  </ul>
-{/if}
+<header>
+  {#if categories.length === 0}
+    <p>pas de quizz disponible</p>
+  {:else}
+    
+    <a href="./form">
+      <h1>proposer un quiz ?</h1>
+    </a>
+  {/if}
+</header>
+<main>
+  {#if categories.length > 0}
+    <ul>
+      {#each categories as category (category.id)}
+        <li>
+          <a href={`./categories/name/${category.name}`}>
+            <CardHome {category} />
+          </a>
+        </li>
+      {/each}
+    </ul>
+  {/if}
+</main>
+
+<style>
+  header {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  main {
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    margin-left: 3vh;
+  }
+  h1 {
+    font-size: 50px;
+    background-color: #9c11b8;
+    color: white;
+    padding: 20px 40px 20px 40px;
+    border-radius: 50px;
+    box-shadow: 5px 9px 11px 2px #000000;
+  }
+  h1:hover {
+    background-color: #4a0857;
+    box-shadow: none;
+  }
+  a {
+    text-decoration: none;
+    color: black;
+  }
+
+  ul
+   {
+    display: flex;
+    flex-wrap: wrap;
+    list-style: none;
+   
+    gap: 30px;
+    color: #4a0857;
+    width: 100%;
+    margin-left: 30px;
+    margin-right: 30px;
+  }
+</style>
