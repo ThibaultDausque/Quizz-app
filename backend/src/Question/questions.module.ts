9d5024299f4d2478e0f23f728b7bbb91c67fa046
Armaiic <144806001+Armaiic@users.noreply.github.com>
2024-02-29 11:18:48 +0100

    add table choice in database (#106)

--- backend/src/Question/questions.module.ts
+++ backend/src/Question/questions.module.ts
@@ -2,15 +2,17 @@ import { Module } from '@nestjs/common';
 import { QuestionsService } from './questions.service';
 import { Question } from './questions.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChoiceModule } from 'src/Choice/choice.module';
 
 //create a QuestionsModule class and import the TypeOrmModule.forFeature() method.
-@Module({ 
-       // 1. Import the TypeOrmModule.forFeature() method and pass in the Question entity:
-        imports: [TypeOrmModule.forFeature([Question])],
-       // 2. Provide the QuestionsService class:
+@Module({
+        // 1. Import the TypeOrmModule.forFeature() method and pass in the Question entity:
+        imports: [TypeOrmModule.forFeature([Question]),
+                ChoiceModule],
+        // 2. Provide the QuestionsService class:
         providers: [QuestionsService],
         // 3. Export the QuestionsService class:
         exports: [QuestionsService],
 })
 // 4. Export the QuestionsModule class:
-export class QuestionsModule {}
+export class QuestionsModule { }
