b08f672126cf8f709be1fe441777551571febe31
Steph4477 <110528810+Steph4477@users.noreply.github.com>
2024-03-01 11:16:29 +0100

    add popup after send the form (#112)
    Co-authored-by: Steph4477 <morelstephan4026@neuf.fr>

---
+++ front/src/lib/components/PopupForm.svelte
@@ -0,0 +1,44 @@
+<script>
+    export let show = false;
+    export let message = "";
+</script>
+
+{#if show}
+    <div class="popup">
+        <div class="popup-content">
+            <h2>{message}</h2>
+            <button on:click={() => (show = false)}>Fermer</button>
+        </div>
+    </div>
+{/if}
+
+<style>
+    .popup {
+        position: fixed;
+        z-index: 1;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        overflow: auto;
+        background-color: rgba(0, 0, 0, 0.4);
+    }
+    .popup-content {
+        background-color: #fefefe;
+        margin: 15% auto;
+        padding: 20px;
+        border: 1px solid #888;
+        width: 80%;
+    }
+    button {
+        margin-top: 20px;
+        font-size: 20px;
+        padding: 10px 20px;
+        background-color: #9c11b8;
+        border-radius: 50px;
+        color: white;
+    }
+    button:hover {
+        background-color: #4a0857;
+    }
+</style>
